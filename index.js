0a44b498f83e170844e2f2c8ae7f22a4fd3d2352
Mathias Buus <mathiasbuus@gmail.com>
2013-06-20 23:41:26 +0200

    packet splitting and back pressuring

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var EXTENSION = 0;
 var VERSION   = 1;
 var UINT16    = 0xffff;
 var ID_MASK   = 0xf << 4;
+var MTU       = 1400;
 
 var PACKET_DATA  = 0 << 4;
 var PACKET_FIN   = 1 << 4;
@@ -25,7 +26,7 @@ var MIN_PACKET_SIZE = 20;
 var DEFAULT_WINDOW_SIZE = 1 << 18;
 var CLOSE_GRACE = 5000;
 
-var BUFFER_MAX = 512;
+var BUFFER_SIZE = 512;
 
 var uint32 = function(n) {
 	return n >>> 0;
@@ -80,11 +81,13 @@ var Connection = function(port, host, socket, syn) {
 	this.host = host;
 	this.socket = socket;
 
-	this._outgoing = cyclist(BUFFER_MAX);
-	this._incoming = cyclist(BUFFER_MAX);
+	this._outgoing = cyclist(BUFFER_SIZE);
+	this._incoming = cyclist(BUFFER_SIZE);
 	this._inflightPackets = 0;
+	this._inflightBytes = 0;
 	this._inflightTimeout = 500000;
 	this._stack = [];
+	this._ondrain = noop;
 
 	if (syn) {
 		this.seq = (Math.random() * UINT16) | 0;
@@ -145,9 +148,17 @@ Connection.prototype.address = function() {
 
 Connection.prototype._read = noop;
 
-Connection.prototype._write = function(data, enc, callback) { // TODO: check size against MTU
+Connection.prototype._write = function(data, enc, callback) {
 	if (this.readyState === CONNECTING) return this._stack.push(this._write.bind(this, data, enc, callback));
-	this._sendPacket(PACKET_DATA, this.sendId, data, callback);
+
+	for (var i = 0; i < data.length; i += MTU) {
+		var flushed = this._sendPacket(PACKET_DATA, this.sendId, data.slice(i, i+MTU), null);
+		if (flushed) continue;
+		this._ondrain = i+MTU < data.length ? this._write.bind(this, data.slice(i+MTU), enc, callback) : callback;
+		return;
+	}
+
+	callback();
 };
 
 Connection.prototype._sendFin = function(callback) {
@@ -156,38 +167,15 @@ Connection.prototype._sendFin = function(callback) {
 };
 
 Connection.prototype._sendAck = function() {
-	this._send(this._packet(PACKET_STATE, this.sendId, null, null));
+	this._sendPacket(PACKET_STATE, this.sendId, null, null);
 };
 
 Connection.prototype._sendPacket = function(id, connection, data, callback) {
-	var packet = this._packet(id, connection, data, callback);
-	this._outgoing.put(packet.seq, packet);
-	this._send(packet);
-};
-
-Connection.prototype._send = function(packet) {
-	var message = packetToBuffer(packet);
-	this._inflightPackets++;
-	this.socket.send(message, 0, message.length, this.port, this.host);
-};
-
-Connection.prototype._checkTimeout = function() {
-	for (var i = 0; i < this._inflightPackets; i++) {
-		var packet = this._outgoing.get(this.seq - i - 1);
-		if (!packet) continue;
-		var now = timestamp();
-		if (uint32(now - packet.sent) < this._inflightTimeout) continue;
-		packet.sent = now;
-		this._send(packet);
-		this.emit('packetresend', packet);
-	}
-};
-
-Connection.prototype._packet = function(id, connection, data, callback) {
 	var now = timestamp();
 	var seq = this.seq;
 	this.seq = uint16(this.seq+1);
-	return {
+
+	var packet = {
 		id: id,
 		connection: connection,
 		timestamp: now,
@@ -199,17 +187,47 @@ Connection.prototype._packet = function(id, connection, data, callback) {
 		sent: now,
 		callback: callback
 	};
+
+	if (id !== PACKET_STATE) this._outgoing.put(packet.seq, packet);
+	if (data) this._inflightBytes += data.length;
+
+	this._inflightPackets++;
+	this._send(packet);
+
+	return this._inflightBytes < DEFAULT_WINDOW_SIZE || this._inflightPackets < BUFFER_SIZE-10;
+};
+
+Connection.prototype._send = function(packet) {
+	var message = packetToBuffer(packet);
+	this.socket.send(message, 0, message.length, this.port, this.host);
+};
+
+Connection.prototype._checkTimeout = function() {
+	for (var i = 0; i < this._inflightPackets; i++) {
+		var packet = this._outgoing.get(this.seq - i - 1);
+		if (!packet) continue;
+		var now = timestamp();
+		if (uint32(now - packet.sent) < this._inflightTimeout) continue;
+		packet.sent = now;
+		this._send(packet);
+	}
 };
 
 Connection.prototype._recvAck = function(seq) {
 	var prevAcked = uint16(this.seq - this._inflightPackets - 1); // last packet that was acked
 	var acks = uint16(seq - prevAcked); // amount of acks we just recv
-	if (acks >= BUFFER_MAX) return; // sanity check
+	if (acks >= BUFFER_SIZE) return; // sanity check
 
 	for (var i = 0; i < acks; i++) {
 		this._inflightPackets--;
 		var packet = this._outgoing.del(prevAcked+i+1);
+		if (packet && packet.data) this._inflightBytes -= packet.data.length;
 		if (packet && packet.callback) packet.callback();
+	}
+
+	if (!this._inflightBytes) {
+		process.nextTick(this._ondrain);
+		this._ondrain = noop;
 	}
 };
 
@@ -229,7 +247,7 @@ Connection.prototype._recvPacket = function(packet) {
 		if (!packet) return;
 	}
 
-	if (uint16(packet.seq - this.ack) >= BUFFER_MAX) return this._sendAck(); // old packet
+	if (uint16(packet.seq - this.ack) >= BUFFER_SIZE) return this._sendAck(); // old packet
 	this._incoming.put(packet.seq, packet);
 
 	var shouldAck = false;
